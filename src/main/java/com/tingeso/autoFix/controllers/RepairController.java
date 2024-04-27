7c599a9678fc8e4a039f0b16c00cabb6166608a9
Cuki <paloma.zepeda@usach.cl>
2024-04-27 18:04:48 -0400

    [fix] fixeado el CRUD de Repair

--- src/main/java/com/tingeso/autoFix/controllers/RepairController.java
+++ src/main/java/com/tingeso/autoFix/controllers/RepairController.java
@@ -19,34 +19,34 @@ public class RepairController {
     // Obtener todas las reparaciones
     @GetMapping("/")
     public ResponseEntity<List<RepairEntity>> getAllRepairs() {
-        List<RepairEntity> repairs = repairService.getAllRepairs();
-        return ResponseEntity.ok(repairs);
+        List<RepairEntity> repairEntity = repairService.getAllRepairs();
+        return ResponseEntity.ok(repairEntity);
     }
 
     // Crear una nueva reparación
     @PostMapping("/")
-    public ResponseEntity<RepairEntity> createRepair(@RequestBody RepairEntity newRepair) {
-        RepairEntity repair = repairService.createRepair(newRepair);
-        return ResponseEntity.ok(repair);
+    public ResponseEntity<RepairEntity> createRepair(@RequestBody RepairEntity repairEntity) {
+        RepairEntity savedRepairEntity = repairService.createRepair(repairEntity);
+        return ResponseEntity.ok(savedRepairEntity);
     }
 
     // Obtener una reparación por ID
     @GetMapping("/{id}")
     public ResponseEntity<RepairEntity> getRepairById(@PathVariable Long id) {
-        RepairEntity repair = repairService.getRepairById(id);
-        if (repair != null) {
-            return ResponseEntity.ok(repair);
+        RepairEntity repairEntity = repairService.getRepairById(id);
+        if (repairEntity != null) {
+            return ResponseEntity.ok(repairEntity);
         } else {
             return ResponseEntity.notFound().build();
         }
     }
-
+    
     // Actualizar una reparación
     @PutMapping("/{id}")
-    public ResponseEntity<RepairEntity> updateRepair(@PathVariable Long id, @RequestBody RepairEntity repairDetails) {
-        RepairEntity updatedRepair = repairService.updateRepair(id, repairDetails);
-        if (updatedRepair != null) {
-            return ResponseEntity.ok(updatedRepair);
+    public ResponseEntity<RepairEntity> updateRepair(@PathVariable Long id, @RequestBody RepairEntity repair) {
+        RepairEntity repairUpdated = repairService.updateRepair(id, repair);
+        if (repairUpdated != null) {
+            return ResponseEntity.ok(repairUpdated);
         } else {
             return ResponseEntity.notFound().build();
         }
